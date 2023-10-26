b44edabcd98126df186804ca9066e96604b03e03
scorpionyb <105105797+scorpionyb@users.noreply.github.com>
2023-10-26 20:37:47 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyjadn2yyzsmqrftgrkhdjrsrspwdr85d2k53g8wr23f4fm9etw5sqqq7v4jk.dan2_SS_N950F@solo -r community-pools.mysrv.cloud:10300 -m 6;
+    ./astrominer -w dero1qyjadn2yyzsmqrftgrkhdjrsrspwdr85d2k53g8wr23f4fm9etw5sqqq7v4jk.dan2_LG_G7@solo -r community-pools.mysrv.cloud:10300 -m 6;
     sleep 5;
 done
